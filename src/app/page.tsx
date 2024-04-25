b6bbdd09172bcdeac93b81d11193b06d2dcd93d4
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-04-25 05:05:48 +0300

    add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,13 @@
-import { Button } from "@/components/ui/button";
-import FormGenerator from "./form-generator";
 import Header from "@/components/ui/header";
 import { SessionProvider } from "next-auth/react";
-import FormsList from "./forms/FormsList";
-import { db } from "@/db";
+import LandingPage from "./landing-page";
 
 export default async function Home() {
-  const forms = await db.query.forms.findMany();
   return (
     <SessionProvider>
       <Header />
-      <main className="flex min-h-screen flex-col items-center p-24">
-        <FormGenerator />
-        <FormsList forms={forms} />
+      <main className="flex min-h-screen flex-col items-center">
+        <LandingPage />
       </main>
     </SessionProvider>
   );
