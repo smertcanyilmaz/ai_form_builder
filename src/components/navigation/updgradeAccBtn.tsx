5b3202b9ad54d0a92ab40e401c6b83fe3b16c11b
smertcanyilmaz <yilmazsmertcan@gmail.com>
2024-05-09 05:46:19 +0300

    return back version

--- src/components/navigation/updgradeAccBtn.tsx
+++ src/components/navigation/updgradeAccBtn.tsx
@@ -1,38 +1,38 @@
-// import React from "react";
-// import Link from "next/link";
-// import { getUserForms } from "@/app/actions/getUserForms";
-// import { MAX_FREE_FROMS } from "@/lib/utils";
-// import ProgressBar from "../progressBar";
-// import SubscribeBtn from "@/app/subscription/SubscribeBtn";
-// import { auth } from "@/auth";
-// import { getUserSubscription } from "@/app/actions/userSubscriptions";
+import React from "react";
+import Link from "next/link";
+import { getUserForms } from "@/app/actions/getUserForms";
+import { MAX_FREE_FROMS } from "@/lib/utils";
+import ProgressBar from "../progressBar";
+import SubscribeBtn from "@/app/subscription/SubscribeBtn";
+import { auth } from "@/auth";
+import { getUserSubscription } from "@/app/actions/userSubscriptions";
 
-// type Props = {};
+type Props = {};
 
-// export default async function UpdgradeAccBtn(props: Props) {
-//   const session = await auth();
-//   const userId = session?.user?.id;
-//   if (!userId) {
-//     return null;
-//   }
-//   const subscription = await getUserSubscription({ userId });
-//   if (subscription) {
-//     return null;
-//   }
-//   const forms = await getUserForms();
-//   const formCount = forms.length;
-//   const percent = (formCount / MAX_FREE_FROMS) * 100;
+export default async function UpdgradeAccBtn(props: Props) {
+  const session = await auth();
+  const userId = session?.user?.id;
+  if (!userId) {
+    return null;
+  }
+  const subscription = await getUserSubscription({ userId });
+  if (subscription) {
+    return null;
+  }
+  const forms = await getUserForms();
+  const formCount = forms.length;
+  const percent = (formCount / MAX_FREE_FROMS) * 100;
 
-//   return (
-//     <div className="p-4 mb-4 text-left text-xs">
-//       <ProgressBar value={percent} />
-//       <p className="mt-2">
-//         {formCount} out of {MAX_FREE_FROMS} forms generated.
-//       </p>
-//       <p>
-//         <SubscribeBtn price="price_1P9yWFDRdzzh1Z4Q1gV68TmD" userId={userId} />{" "}
-//         for unlimited forms.
-//       </p>
-//     </div>
-//   );
-// }
+  return (
+    <div className="p-4 mb-4 text-left text-xs">
+      <ProgressBar value={percent} />
+      <p className="mt-2">
+        {formCount} out of {MAX_FREE_FROMS} forms generated.
+      </p>
+      <p>
+        <SubscribeBtn price="price_1P9yWFDRdzzh1Z4Q1gV68TmD" userId={userId} />{" "}
+        for unlimited forms.
+      </p>
+    </div>
+  );
+}
